959dc5c7e712760f73a43a1869c04f951bf9473b
Axel <axelbmichaud@gmail.com>
2021-05-15 11:22:58 -0400

    Run on the discord bot

--- Program.cs
+++ Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
 
-namespace DiscordBot
+namespace MyFirstBot
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Test 5");
+
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = "ODQzMTA4MjYxOTk3OTY5NDE5.YJ_DwQ.sQpXS2nfc_PzNSJrgIeJG9ZtCOY",
+                TokenType = TokenType.Bot
+            });
+
+            discord.MessageCreated += async (s, e) =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("ping"))
+                    await e.Message.RespondAsync("pong! 5");
+
+            };
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
         }
     }
 }
